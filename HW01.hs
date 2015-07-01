wangqr/CIS194

commit 5a0c401d2aec0a0d27c7ffa2e72e630c322d518a

replace div-mod with quot-rem in HW01

HW01.hs
@@ -8,11 +8,11 @@ import Data.List
 
 -- Get the last digit from a number
 lastDigit :: Integer -> Integer
-lastDigit = (`mod` 10)
+lastDigit = (`quot` 10)
 
 -- Drop the last digit from a number
 dropLastDigit :: Integer -> Integer
-dropLastDigit = (`div` 10)
+dropLastDigit = (`rem` 10)
 
 -- Exercise 2 -----------------------------------------
 
@@ -39,7 +39,7 @@ sumDigits = sum . map (sum . toRevDigits)
 
 -- Validate a credit card number using the above functions.
 luhn :: Integer -> Bool
-luhn = (0==) . (`mod` 10) . sumDigits . doubleEveryOther . toRevDigits
+luhn = (0==) . (`rem` 10) . sumDigits . doubleEveryOther . toRevDigits
 
 -- Exercise 6 -----------------------------------------
 

commit 18e5235520126465f7f6e371b50b6ab58b918669

fixed some mistakes introduced in commit 5a0c401
rewrite a function with `zipWith`

HW01.hs
@@ -8,11 +8,11 @@ import Data.List
 
 -- Get the last digit from a number
 lastDigit :: Integer -> Integer
-lastDigit = (`quot` 10)
+lastDigit = (`rem` 10)
 
 -- Drop the last digit from a number
 dropLastDigit :: Integer -> Integer
-dropLastDigit = (`rem` 10)
+dropLastDigit = (`quot` 10)
 
 -- Exercise 2 -----------------------------------------
 
@@ -25,8 +25,7 @@ toRevDigits x
 
 -- Double every second number in a list starting on the left.
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther (x:y:xs) = x:(2*y):(doubleEveryOther xs)
-doubleEveryOther xs       = xs
+doubleEveryOther = zipWith (*) $ cycle [1, 2]
 
 -- Exercise 4 -----------------------------------------
 